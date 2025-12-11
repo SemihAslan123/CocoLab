b7d44b21cccb07ebfa3cb2713d47e03789cf3a9e
SemihAslan123 <semihaslan2210@gmail.com>
2025-12-11 22:31:27 +0100

    correction port 587

--- backend/server.js
+++ backend/server.js
@@ -34,11 +34,14 @@ app.use(express.json()); // Pour analyser le corps des requêtes en JSON
 // Configuration Nodemailer
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // utilise SSL
+    port: 587,
+    secure: false, // false pour le port 587 (on utilise STARTTLS)
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false // Option de secours si le certificat Google fait des siennes
     }
 });
 
